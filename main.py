43ec2c321a277ed78cfe239993b4c8043d38829b
Baptiste Vert <baptiste.vert@outlook.fr>
2023-09-06 12:19:00 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from src.utils.ask_question_to_pdf import ask_question_to_pdf
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/image")
+def image():
+    return "<p>l</p>"
+  
+
+@app.route("/prompt",methods=["GET", "PATCH", "DELETE", "POST"])
+def prompt():
+    print(ask_question_to_pdf("Are you a teacher"))
+    return {"answer": ask_question_to_pdf("/prompt")}
+
+
+
